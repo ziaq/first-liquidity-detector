fe45bb33cf84907b09ffae7803934248cf3c55d9
Roma <zixrom@gmail.com>
2023-05-01 16:00:06 +0000

    fix openTrading method processing

--- src/utils/getTokensFromTransaction.js
+++ src/utils/getTokensFromTransaction.js
@@ -3,14 +3,12 @@ const uniswapV2RouterAbi = require('../../abi/uniswapV2Router.json');
 const openTradingAbi = require('../../abi/openTrading.json');
 const logger = require('./logger');
 
-async function getTokensFromTransaction(transaction, addLiquiditySignature) {
+function getTokensFromTransaction(transaction, addLiquiditySignature) {
   let tokenA, tokenB;
 
   if (addLiquiditySignature === '0xc9567bf9') { // openTrading
-    const tokenContractInterface = new ethers.utils.Interface(openTradingAbi);
-    const decodedTx = tokenContractInterface.parseTransaction(transaction);
-    tokenA = decodedTx.args.token0;
-    tokenB = decodedTx.args.token1;
+    tokenA = transaction.to;
+    tokenB = '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2'; // WETH address
 
   } else {
     const tokenContractInterface = new ethers.utils.Interface(uniswapV2RouterAbi);
